90ba40f4a3dbf1de10f7b69a8ff91d488f74defe
Bryan Louie Martinez <>
2023-10-21 18:44:15 +0800

    Add stack example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-(async () => {})();
+import { Stack } from './data-structures/stack';
+
+(async () => {
+  // Stack example - Reversing a word
+  const word = 'Hello World';
+  const stack = new Stack<string>(word.length);
+
+  word.split('').forEach((letter) => {
+    stack.push(letter);
+  });
+
+  const reversedString: string[] = [];
+
+  for (let i = 0; i < word.length; i++) {
+    reversedString.push(stack.pop());
+  }
+
+  console.log(reversedString.join('')); // should be dlroW olleH
+})();
