6c28f0e82dfe2fb99bfdc6258a08e8b9251f80e2
Bryan Louie Martinez <>
2023-10-25 21:25:21 +0800

    Add inserts for CircularLinkedList

--- src/data-structures/linked-list/circular-linked-list.spec.ts
+++ src/data-structures/linked-list/circular-linked-list.spec.ts
@@ -4,15 +4,42 @@ import { LinkedListNode } from './linked-list';
 describe('CircularLinkedList', () => {
   describe('insertStart', () => {
     it('should insert', () => {
-      const node1 = new LinkedListNode(1);
-      const list = new CircularLinkedList(node1);
-      list.insertStart(2);
+      const list = new CircularLinkedList();
 
+      list.insertStart(1);
+      expect(list.display()).toStrictEqual([1]);
+
+      list.insertStart(2);
       expect(list.display()).toStrictEqual([2, 1]);
 
       list.insertStart(3);
-
       expect(list.display()).toStrictEqual([3, 2, 1]);
+    });
+  });
+  describe('insertEnd', () => {
+    it('should insert', () => {
+      const list = new CircularLinkedList();
+
+      list.insertEnd(1);
+      expect(list.display()).toStrictEqual([1]);
+
+      list.insertEnd(2);
+      expect(list.display()).toStrictEqual([1, 2]);
+
+      list.insertEnd(3);
+      expect(list.display()).toStrictEqual([1, 2, 3]);
+    });
+  });
+  describe('insertAfter', () => {
+    it('should insert', () => {
+      const node1 = new LinkedListNode(1);
+      const list = new CircularLinkedList(node1);
+
+      list.insertAfter(node1, 3);
+      expect(list.display()).toStrictEqual([1, 3]);
+
+      list.insertAfter(node1, 2);
+      expect(list.display()).toStrictEqual([1, 2, 3]);
     });
   });
 });
--- src/data-structures/linked-list/circular-linked-list.ts
+++ src/data-structures/linked-list/circular-linked-list.ts
@@ -13,7 +13,9 @@ export class CircularLinkedList<T> extends LinkedList<T> {
   constructor(head: LinkedListNode<T> = null) {
     super(head);
     this.head = head;
-    this.head.next = head;
+    if (this.head) {
+      this.head.next = head;
+    }
   }
 
   public override display(): T[] {
@@ -22,11 +24,8 @@ export class CircularLinkedList<T> extends LinkedList<T> {
     let node = this.head;
 
     while (node && node.next !== this.head) {
-      console.log(node);
       result.push(node.data);
       node = node.next;
-      console.log(node);
-      console.log(this.head);
     }
     result.push(node.data);
 
@@ -36,23 +35,67 @@ export class CircularLinkedList<T> extends LinkedList<T> {
   public override insertStart(data: T): LinkedListNode<T> {
     const node = new LinkedListNode(data);
 
-    if (this.head !== null) {
-      node.next = this.head;
+    if (this.head === null) {
+      this.head = node;
+      this.head.next = node;
+      return this.head;
     }
 
     const lastNode = this.getLastNode();
+    node.next = this.head;
     this.head = node;
+    lastNode.next = this.head;
+    return node;
+  }
+
+  public override insertAfter(
+    prevNode: LinkedListNode<T>,
+    data: T,
+  ): LinkedListNode<T> {
+    if (!prevNode) {
+      throw new Error('Previous node cannot be null.');
+    }
+
+    const node = new LinkedListNode(data);
+
+    const lastNode = this.getLastNode();
+
+    if (prevNode === lastNode) {
+      lastNode.next = node;
+      node.next = this.head;
+    } else {
+      node.next = prevNode.next;
+      prevNode.next = node;
+    }
+    return node;
+  }
+
+  public override insertEnd(data: T): LinkedListNode<T> {
+    const node = new LinkedListNode(data);
+
+    if (this.head === null) {
+      this.head = node;
+      this.head.next = node;
+      return this.head;
+    }
+
+    const lastNode = this.getLastNode();
     lastNode.next = node;
+    node.next = this.head;
     return node;
   }
 
   private getLastNode(): LinkedListNode<T> {
+    if (this.head === null) {
+      return;
+    }
+
     let currentNode = this.head;
 
     while (currentNode.next !== this.head) {
       currentNode = currentNode.next;
     }
 
-    return currentNode.next;
+    return currentNode;
   }
 }
