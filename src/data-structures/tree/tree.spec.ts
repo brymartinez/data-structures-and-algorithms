83b40dcae16dc85cfd8d379b23542d0172c929c0
Bryan Louie Martinez <>
2023-10-27 16:34:43 +0800

    Create tree helper functions

--- src/data-structures/tree/tree.spec.ts
+++ src/data-structures/tree/tree.spec.ts
@@ -1,4 +1,5 @@
 import { TreeNode, Tree } from './tree';
+import { height, isFull, isPerfect } from './tree-helpers';
 
 describe('Tree', () => {
   describe('inorder', () => {
@@ -12,6 +13,32 @@ describe('Tree', () => {
 
       // expect(tree.inorder(tree.root)).toBeTruthy();
       expect(tree.postorder(tree.root)).toBeTruthy();
+    });
+  });
+  describe('helpers', () => {
+    describe('isFull', () => {
+      it('should return true', () => {
+        const root = new TreeNode(1);
+        const tree = new Tree(root);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+
+        expect(isFull(tree.root)).toBe(true);
+        tree.root.left.left = new TreeNode(4);
+        expect(isFull(tree.root)).toBe(false);
+      });
+    });
+    describe('isPerfect', () => {
+      it('should return true', () => {
+        const root = new TreeNode(1);
+        const tree = new Tree(root);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+
+        expect(isPerfect(tree.root, height(tree.root))).toBe(true);
+        tree.root.left.left = new TreeNode(4);
+        expect(isFull(tree.root)).toBe(false);
+      });
     });
   });
 });
